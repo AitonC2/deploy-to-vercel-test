dfa29dffaa4ebcfee8e0e60558f80199c779b6aa
Aiton A Cumbi <81229223+AitonC2@users.noreply.github.com>
2022-06-06 15:33:24 +0200

    Update product.js

--- api/product.js
+++ api/product.js
@@ -4,10 +4,10 @@ const router = express.Router();
 router.get('/', async(req, res)=>{
 
     try{
-        res.send(`
-            <h1>Hello Mom!</h1>
-            <h2>I'm a backend nodeJS developer now!</h2>
-        `)
+        res.json({
+            status: 200,
+            message: "Data retrieved sucessfully"
+        });
     }catch(err){
         console.log(err)
         return res.status(500).send("server error")
